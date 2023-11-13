ab8eb777f1bcf249d375ac5e3b2ce136f73c9c78
Luis <llopezgillue@gmail.com>
2023-11-13 13:31:53 +0100

    "mejorando el welcome"

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
